70757cd98753e0a5ddcce0d78dd7df21af66b556
Alaa Zayood <alaazayood@gmail.com>
2025-09-07 10:19:35 +0300

    Fix image paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/menu-app-site/', // 👈 لازم نفس اسم الريبو بالضبط
+
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
