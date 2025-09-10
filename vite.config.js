8472a053691812974b51f0ed5e4857c1fe5a974d
Alaa Zayood <alaazayood@gmail.com>
2025-09-10 08:03:49 +0300

    feat: clean tenant paths via Vercel rewrites

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/menu-app-site/', // 👈 لازم نفس اسم الريبو بالضبط
+  base: '/', // 👈 لازم نفس اسم الريبو بالضبط
 
   plugins: [vue()],
   resolve: {
